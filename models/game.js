bc2a90262a747c4a4ff0c653f6c85758b519ddaa
ManarSwar97 <manarswar97@gmail.com>
2025-05-16 12:59:18 +0300

    Connect sign in page to the home page

--- models/game.js
+++ models/game.js
@@ -4,7 +4,7 @@ const gameSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'groups',
     },
-    groupName:{
+    gameName:{
         type: String,
     }
 })
