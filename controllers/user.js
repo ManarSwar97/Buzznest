63f655888e9d7d86552fcf5dffb7674e40deb57c
noor96faisal <n.fai2@icloud.com>
2025-05-16 18:39:21 +0300

    fixed the user.js profile route

--- controllers/user.js
+++ controllers/user.js
@@ -1,94 +1,114 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
-const User = require("../models/user")
-const bcrypt = require("bcrypt")
-router.get("/sign-up", async(req, res)=>{
-    res.render("auth/sign-up.ejs")
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+router.get('/sign-up', async (req, res) => {
+  res.render('auth/sign-up.ejs')
 })
-router.post("/sign-up", async (req, res) => {
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const username = req.body.username;
-    const password = req.body.password;
-    const confirmPassword = req.body.confirmPassword;
-    const email = req.body.email;
-    const image = req.body.image;
+router.post('/sign-up', async (req, res) => {
+  const firstName = req.body.firstName
+  const lastName = req.body.lastName
+  const username = req.body.username
+  const password = req.body.password
+  const confirmPassword = req.body.confirmPassword
+  const email = req.body.email
+  const image = req.body.image
 
-    const existingUsername = await User.findOne({ username });
-    const existingEmail = await User.findOne({ email });
+  const existingUsername = await User.findOne({ username })
+  const existingEmail = await User.findOne({ email })
 
-    if (existingUsername) {
-        return res.send("Username already taken.")
-    }
+  if (existingUsername) {
+    return res.send('Username already taken.')
+  }
 
-    if (existingEmail) {
-        return res.send("Email already registered.")
-    }
+  if (existingEmail) {
+    return res.send('Email already registered.')
+  }
 
-    if (password !== confirmPassword) {
-        return res.send("Password and Confirm password are not the same.")
-    }
-    const hashedPass = bcrypt.hashSync(req.body.password, 12)
-    req.body.password = hashedPass
+  if (password !== confirmPassword) {
+    return res.send('Password and Confirm password are not the same.')
+  }
+  const hashedPass = bcrypt.hashSync(req.body.password, 12)
+  req.body.password = hashedPass
 
-    const newUser = await User.create({
-        firstName,
-        lastName,
-        email,
-        username,
-        password: req.body.password,
-        image,
-
-    });
-
-    req.session.newUser = {
-        firstName: newUser.firstName,
-        lastName: newUser.lastName,
-        email: newUser.email,
-        username: newUser.username,
-        image: newUser.image,
-    };
-
-    req.session.save(() => {
-        res.redirect("/");
-    });
-});
-
-router.get("/sign-in", async(req, res)=>{
-    res.render("auth/sign-in.ejs")
-})
-router.post("/sign-in", async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    const existingUser = await User.findOne({ username });
-    if (!existingUser) {
-        return res.send("Invalid Username");
-    }
-
-    const isPasswordValid = await bcrypt.compare(password, existingUser.password);
-    if (!isPasswordValid) {
-        return res.send("Invalid Password");
-    }
-    req.session.user = {
-        username: existingUser.username,
-        _id: existingUser._id
-    };
-    res.redirect("/");
-});
-
-router.get("/sign-out", (req, res) => {
-    req.session.destroy()
-    res.redirect("/")
+  const newUser = await User.create({
+    firstName,
+    lastName,
+    email,
+    username,
+    password: req.body.password,
+    image
   })
 
+  req.session.newUser = {
+    firstName: newUser.firstName,
+    lastName: newUser.lastName,
+    email: newUser.email,
+    username: newUser.username,
+    image: newUser.image
+  }
 
-  //profile
-  //I took a reference from skyrockit lesson and some previous lessons.
-router.get("/profile", async (req,res)=>{
-    const currentUser = await User.findById(req.session.user._id);
-    const userProfile = currentUser;
-    res.render("profile/profile.ejs",{user: userProfile})
+  req.session.save(() => {
+    res.redirect('/')
+  })
+})
+
+router.get('/sign-in', async (req, res) => {
+  res.render('auth/sign-in.ejs')
+})
+router.post('/sign-in', async (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  const existingUser = await User.findOne({ username })
+  if (!existingUser) {
+    return res.send('Invalid Username')
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, existingUser.password)
+  if (!isPasswordValid) {
+    return res.send('Invalid Password')
+  }
+  req.session.user = {
+    username: existingUser.username,
+    _id: existingUser._id
+  }
+  res.redirect('/')
+})
+
+router.get('/sign-out', (req, res) => {
+  req.session.destroy()
+  res.redirect('/')
+})
+
+// PROFILE
+// I looked at a reference from MEN STACK AUTH LESSON /controllers/auth.js
+//and references from https://stackoverflow.com/questions/50220376/try-catch-setup-in-node-js
+//and this https://javascript.info/try-catch
+//https://www.bennadel.com/blog/2154-i-finally-understand-the-finally-part-of-a-try-catch-control-statemen
+// //https://stackoverflow.com/questions/42013104/placement-of-catch-before-and-after-thent.htm
+
+
+router.get('/profile', (req, res) => {
+  if (!req.session.user) {
+    res.redirect('/sign-in')
+    return
+  }
+
+  User.findById(req.session.user._id)
+    .then((user) => {
+      if (!user) {
+        res.redirect('/')
+        return
+      }
+  
+
+      res.render('profile/profile.ejs', { user })
+    })
+    .catch((error) => {
+      console.log(error)
+      res.send('Something went wrong!')
+    })
 })
 
 module.exports = router
