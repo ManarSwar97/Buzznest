fca5887a6c24dc38270a9f18f96e76c48022b17d
noor96faisal <n.fai2@icloud.com>
2025-05-20 15:04:11 +0300

    update

--- models/comment.js
+++ models/comment.js
@@ -1,22 +1,21 @@
 const mongoose = require('mongoose')
 const commentSchema = new mongoose.Schema({
-    post:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'post',
-    },
-    comment:{
-        type: String
-    },
-    group:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'groups',
-    },
-    user:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user',   
-     }
-
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'post'
+  },
+  comment: {
+    type: String
+  },
+  group: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'groups'
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  }
 })
 
-const Comment = mongoose.model("Comment", commentSchema)
+const Comment = mongoose.model('Comment', commentSchema)
 module.exports = Comment
